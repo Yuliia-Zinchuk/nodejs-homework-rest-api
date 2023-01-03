65001554eae9066db129eccfdfeb16f4b76d8415
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2023-01-04 01:00:43 +0200

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const { validateBody } = require("../../middlewares");
 
+const { schemas } = require("../../models/user");
+
 const router = express.Router();
 
 //signup
-//router.post("/register");
+router.post("/register", validateBody(schemas.registerSchema));
 
 module.exports = router;
