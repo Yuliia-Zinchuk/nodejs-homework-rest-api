327b35f320a806690d8d270ea32a1af04a54e239
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2023-01-04 01:01:15 +0200

    update users model

--- models/user.js
+++ models/user.js
@@ -4,6 +4,9 @@ const Joi = require("joi");
 
 const { handleMongooseError } = require("../helpers");
 
+// const emailRegexp =
+//   /([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])/;
+
 const userSchema = new Schema(
   {
     password: {
@@ -13,6 +16,7 @@ const userSchema = new Schema(
     email: {
       type: String,
       required: [true, "Email is required"],
+      // match: emailRegexp,
       unique: true,
     },
     subscription: {
@@ -27,3 +31,30 @@ const userSchema = new Schema(
   },
   { versionKey: false, timestamps: true }
 );
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  // email: Joi.string().pattern(emailRegexp).required(),
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  //email: Joi.string().pattern(emailRegexp).required(),
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
