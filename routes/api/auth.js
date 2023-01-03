1defc139bf3478b9751feea9c303a0e0b805ada2
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2023-01-03 22:25:24 +0200

    create auth.js and user.js

---
+++ routes/api/auth.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const { validateBody } = require("../../middlewares");
+
+const router = express.Router();
+
+//signup
+//router.post("/register");
+
+module.exports = router;
