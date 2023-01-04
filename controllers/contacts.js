2b952f760456257755011450d713f3a23ad10c29
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2023-01-04 17:30:01 +0200

    add pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,12 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }).populate("owner", "name email");
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt, -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
   res.json(result);
 };
 
