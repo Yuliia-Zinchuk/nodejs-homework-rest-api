b353767b3ef6090e9dbc811f4bf53d6270177cb7
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2023-01-04 14:46:20 +0200

    add authenticate

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,30 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 
 const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:id", ctrl.getById);
+router.get("/:id", authenticate, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:id", ctrl.deleteById);
+router.delete("/:id", authenticate, ctrl.deleteById);
 
-router.put("/:id", validateBody(schemas.addSchema), ctrl.updateById);
+router.put(
+  "/:id",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.updateById
+);
 
 router.patch(
   "/:id/favorite",
+  authenticate,
   validateBody(schemas.updateStatusContactSchema),
   ctrl.updateStatusContact
 );
