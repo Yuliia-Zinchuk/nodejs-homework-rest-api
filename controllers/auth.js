bef9c3d728d5858616682575ba06ca384cb015ed
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2023-01-05 19:14:03 +0200

    add to update avatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,10 +2,13 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
 
 const { User } = require("../models/user");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
+const { isError } = require("joi");
 
 const { SECRET_KEY } = process.env;
 
@@ -74,9 +77,24 @@ const logout = async (req, res) => {
   res.status(204).json();
 };
 
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const resultUpload = path.join(avatarsDir, originalname);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", originalname);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+  res.json({
+    avatarURL,
+  });
+};
+
 module.exports = {
   signup: ctrlWrapper(signup),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 
 const { schemas } = require("../../models/user");
 
@@ -17,5 +17,12 @@ router.post("/users/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/users/current", authenticate, ctrl.getCurrent);
 
 router.get("/users/logout", authenticate, ctrl.logout);
+
+router.patch(
+  "/users/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
