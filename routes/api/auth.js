702b0515c79999adb1cc918b547616d5c4d13c1e
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2023-01-06 16:53:54 +0200

    update hw-6

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -11,6 +11,14 @@ const router = express.Router();
 //signup
 router.post("/users/signup", validateBody(schemas.signupSchema), ctrl.signup);
 
+router.get("/users/verify/:verificationToken", ctrl.verify);
+
+router.post(
+  "/users/verify",
+  validateBody(schemas.verifyEmailSchema),
+  ctrl.resendVerifyEmail
+);
+
 //login
 router.post("/users/login", validateBody(schemas.loginSchema), ctrl.login);
 
